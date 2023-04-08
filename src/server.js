6e071684c7324f021223e2cfbae48198bf6fc357
Aayush <karkiayush201@gmail.com>
2023-04-08 21:51:31 +0545

    initial commit

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const cors = require("cors");
+const express = require("express");
+const prisma = require("./prisma");
+const app = express();
+
+const server = http.createServer(app);
+
+app.use(cors());
+app.post("/user", async (req, res) => {
+  const { userName, email } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      email,
+      userName,
+    },
+  });
+  return res.status(201).json({ message: "User created Successfully", user });
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany({});
+  return res.status(200).json({ message: "All Users", users });
+});
+
+server.listen(3000, () => {
+  console.log("Listening on http://localhost:8000");
+});
